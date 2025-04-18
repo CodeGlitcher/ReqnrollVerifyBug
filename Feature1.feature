860e8eedac4b04a7b96602ffc0d61c704907f39c
Rob Gerritsen <Rob.Gerritsen@nslogin.nl>
2025-04-18 11:12:12 +0200

    Add project files.

---
+++ Feature1.feature
@@ -0,0 +1,7 @@
+﻿Feature: Feature1
+
+Scenario: First
+	When Verify is called there should be no exception
+
+Scenario: Second
+	When Verify is called there should be no exception
---
+++ When.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using Reqnroll;
+
+namespace ReqnrollVerifyBug
+{
+    [Binding]
+    internal class When
+    {
+        private readonly VerifySettings _settings;
+
+        public When(VerifySettings settings)
+        {
+            _settings = settings;
+        }
+        [When("Verify is called there should be no exception")]
+        public async Task WhenVerifyIsCalledThereShouldBeNoException()
+        {
+            await Verify(1, _settings);
+        }
+
+    }
+}
